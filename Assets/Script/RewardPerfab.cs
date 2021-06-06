e5bb8c099dbef2ff11883ea426b28f9d28153025
caishen233 <caishen@89trillion.com>
2021-06-06 16:49:15 +0800

    第五题

---
+++ Assets/Script/RewardPerfab.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RewardPerfab : MonoBehaviour
+{
+    public GameObject rewardImage;
+    public GameObject rewardImage2;
+    public Text rankLever;
+    private int trophySum = 6000;
+    private Manager M = new Manager();
+    public void closeRewardButton()
+    {
+        rewardImage.SetActive(false);
+    }
+    public void closeRewardButtonBackGround()
+    {
+        rewardImage2.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
